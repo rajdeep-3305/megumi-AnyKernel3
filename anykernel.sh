b4216d4be97bc93407c70641bbcc3127aa1c6f57
Reina in RainyLand <rainyxeon@gmail.com>
2025-06-19 10:39:29 +0700

    Update anykernel.sh
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Redmi Note 12 Pro (ruby) - Kernel by Wodanesdag / StarifyBuilder
+kernel.string=Redmi Note 12 Pro (ruby) - Kernel by Wodanesdag / RainyXeon
 do.devicecheck=1
 do.modules=0
 do.systemless=1
